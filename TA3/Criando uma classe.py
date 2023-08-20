118889bb3b07f6db62b9ea813fe866be2880a7e3
vanderfnj <121826313+vanderfnj@users.noreply.github.com>
2023-08-19 21:12:45 -0300

    Create Criando uma classe.py
    Preciso aprender a rodar no IDE do phyton

---
+++ TA3/Criando uma classe.py
@@ -0,0 +1,5 @@
+class primeiraclasse:
+    def imprimir_mensagem(self, nome): # Criando um método
+        print(f"Olá (nome), seja bem vindo")
+        objeto1 = primeiraclasse() # instanciando um objeto do tipo primeiraclasse
+        objeto1.imprimir_mensagem('DUDU') #invocando o método
